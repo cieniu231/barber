43c87008b968a5504b42cce900615f410ac7c0fc
cieniu231 <cieniu231@o2.pl>
2023-05-17 20:21:42 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import Navbar  from "./components/navigation";
@@ -6,20 +5,25 @@ import Home from "./components/home"
 import About from "./components/about";
 import Reservation from "./components/reservation";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from './components/ThemeContext'
+
 
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/reservation" element={<Reservation/>} />
-        </Routes>
-    </div>
+      <ThemeProvider>
+          <div className="App">
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/reservation" element={<Reservation/>} />
+            </Routes>
+          </div>
+      </ThemeProvider>
+
   );
 }
 
--- src/components/about.js
+++ src/components/about.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import { useTheme, useThemeUpdate } from './ThemeContext'
 export default function About() {
-    return <h1>About</h1>
+    const darkTheme = useTheme()
+    const toggleTheme = useThemeUpdate()
+    const themeStyles = {
+        backgroundColor: darkTheme ? '#333' : '#CCC',
+        color: darkTheme ? '#CCC' : '#333',
+        padding: '2rem',
+        margin: '2rem'
+    }
+
+    return (
+        <>
+            <button onClick={toggleTheme}>Toggle Theme</button>
+            <h1 style={themeStyles}>About</h1>
+        </>
+    )
 }
